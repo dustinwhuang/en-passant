84eafc048e2f09de6fe86be5da48739aace37780
Dustin Huang <mail@dustinhuang.com>
2017-11-12 03:55:35 -0800

    Remove RowView and do all iterating in BoardView

--- client/src/BoardView.jsx
+++ client/src/BoardView.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import RowView from './RowView.jsx'
+import SquareView from './SquareView.jsx'
 
 const BoardView = ({board, style, handleSquareClick}) => (
   <div className="board">
     <table id="chessboard">
       <tbody>
-        {board.map((row, key) => <RowView row={row} style={style[key]} rowId={8 - key} handleSquareClick={handleSquareClick} key={key} />)}
+        {board.map((row, rowId) => (
+          <tr className={rowId} key={rowId}>
+            {row.map((square, key) => <SquareView
+              square={square}
+              style={style[key]}
+              squareId={String.fromCharCode('a'.charCodeAt()  + key)} 
+              rowId={rowId}
+              handleSquareClick={handleSquareClick}
+              key={key}
+              />)}
+          </tr>)
+        )}
       </tbody>
     </table>
   </div>
